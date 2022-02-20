78db5218c52ac39bcf9a7eec79fb170a1e850fae
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-02-20 22:34:21 +0100

    Creates the Title, POPOSList and Spaces components

---
+++ src/POPOSSpace.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import logo from './logo.svg'
+
+function POPOSSpace() {
+    return(
+        <div>
+            <img src= {'${process.env.PUBLIC_URL}images/public_space.jpg'} 
+            width =' 300'
+            height = '300'
+            alt = 'public_space'
+            
+            />
+    </div>
+    )
+    
+}
+
+export default POPOSSpace;
